a76d632c30ebafca564d5c1c1b4bbb6432665e97
Vanessa Tan <vanessatjx25@gmail.com>
2024-09-23 11:47:34 +0800

    added more insert queries

--- backend/python/insert_queries.py
+++ backend/python/insert_queries.py
@@ -56,3 +56,107 @@ def insert_user(user_info, role_info):
     finally:
         if connection:
             close_db_connection(connection, tunnel)
+
+"""
+appointment_info:
+- patient_id_fk
+- doctor_id_fk
+- date
+- time
+- type
+"""
+
+def insert_appointment(appointment_info):
+    connection = None
+    try:
+        connection, tunnel = get_db_connection()
+
+        with connection.cursor() as cursor:
+            insert_query = """
+            INSERT INTO appointment (patient_id_fk, doctor_id_fk, date, time, status, type)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """
+
+            cursor.execute(insert_query, (appointment_info['patient_id_fk'], appointment_info['doctor_id_fk'], appointment_info['date'], appointment_info['time'], 'pending', appointment_info['type']))
+
+            connection.commit()
+
+        return {"status": "success", "message": "Appointment added successfully."}
+    
+    except Exception as e:
+        if connection:
+            connection.rollback()
+        print(f"Status: error, Message: Error occurred: {str(e)}")
+        #return {"status": "error", "message": f"Error occurred: {str(e)}"}
+    finally:
+        if connection:
+            close_db_connection(connection, tunnel)
+
+"""
+diagnosis_info:
+- patient_id_fk
+- condition_id_fk
+- doctor_id_fk
+- diagnosis_date
+- severity
+"""
+def insert_diagnosis(diagnosis_info):
+    connection = None
+    try:
+        connection, tunnel = get_db_connection()
+
+        with connection.cursor() as cursor:
+            insert_query = """
+            INSERT INTO diagnosis (patient_id_fk, condition_id_fk, doctor_id_fk, diagnosis_date, severity)
+            VALUES (%s, %s, %s, %s, %s)
+            """
+
+            cursor.execute(insert_query, (diagnosis_info['patient_id_fk'], diagnosis_info['condition_id_fk'], diagnosis_info['doctor_id_fk'], diagnosis_info['diagnosis_date'], diagnosis_info['severity']))
+
+            connection.commit()
+        
+        return {"status": "success", "message": "Diagnosis added successfully."}
+
+    except Exception as e:
+        if connection:
+            connection.rollback()
+        print(f"Status: error, Message: Error occurred: {str(e)}")
+        #return {"status": "error", "message": f"Error occurred: {str(e)}"}
+    finally:
+        if connection:
+            close_db_connection(connection, tunnel)
+
+"""
+billing_info:
+- appointment_id_fk
+- amount_due
+- amount_paid
+- billing_date
+- payment_status
+- payment_method
+"""
+def insert_billing(billing_info):
+    connection = None
+    try:
+        connection, tunnel = get_db_connection()
+
+        with connection.cursor() as cursor:
+            insert_query = """
+            INSERT INTO billing (appointment_id_fk, amount_due, amount_paid, billing_date, payment_status, payment_method)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """
+
+            cursor.execute(insert_query, (billing_info['appointment_id_fk'], billing_info['amount_due'], billing_info['amount_paid'], billing_info['billing_date'], billing_info['payment_status'], billing_info['payment_method']))
+
+            connection.commit()
+
+        return {"status": "success", "message": "Billing added successfully."}
+    
+    except Exception as e:
+        if connection:
+            connection.rollback()
+        print(f"Status: error, Message: Error occurred: {str(e)}")
+        #return {"status": "error", "message": f"Error occurred: {str(e)}"}
+    finally:
+        if connection:
+            close_db_connection(connection, tunnel)
